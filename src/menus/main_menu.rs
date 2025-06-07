08a7ffdef770f93d6d46c4998f0567da5a3a3865
Christopher Anderson <christopher.r.anderson@gmail.com>
2025-06-07 00:42:39 -0400

    MO-20: Add title to title screen

--- src/menus/main_menu.rs
+++ src/menus/main_menu.rs
@@ -14,58 +14,77 @@ pub fn spawn_main_menu(mut commands: Commands, asset_server: &AssetServer) {
         Node::default(),
         ClassList::new_with_classes(["globally-center-children", "overlay"]),
         NodeStyleSheet::new(asset_server.load("styles/all.css")),
-        children![(
-            ClassList::new_with_classes(["game-menu"]),
-            Node::default(),
-            NavigableChildren::default(),
-            Children::spawn(SpawnWith(|spawner: &mut ChildSpawner| {
-                spawner.spawn((
-                    ClassList::new_with_classes(["main-menu-heading"]),
-                    Node::default(),
-                    Children::spawn_one(Text::new("Main Menu")),
-                ));
+        children![
+            (
+                Node::default(),
+                ClassList::new_with_classes(["column"]),
+                children![
+                    (
+                        Text::new("Maxx"),
+                        ClassList::new_with_classes(["title-maxx"]),
+                        Transform::from_rotation(Quat::from_rotation_z(-90f32.to_radians())),
+                    ),
+                    (
+                        Text::new("Obliterate"),
+                        ClassList::new_with_classes(["title-obliterate"]),
+                        Transform::from_rotation(Quat::from_rotation_z(90f32.to_radians())),
+                    )
+                ]
+            ),
+            (
+                ClassList::new_with_classes(["game-menu"]),
+                Node::default(),
+                NavigableChildren::default(),
+                Children::spawn(SpawnWith(|spawner: &mut ChildSpawner| {
+                    spawner.spawn((
+                        ClassList::new_with_classes(["main-menu-heading"]),
+                        Node::default(),
+                        Children::spawn_one(Text::new("Main Menu")),
+                    ));
 
-                spawner.spawn((button("Play"), AutoFocus)).observe(
-                    |_trigger: Trigger<ButtonActivate>,
-                     mut selected_mode: ResMut<SelectedGameRunMode>,
-                     mut next_state: ResMut<NextState<AppState>>| {
-                        selected_mode.0 = Some(GameRunMode::Game);
-                        next_state.set(AppState::ResetGameRun);
-                    },
-                );
+                    spawner.spawn((button("Play"), AutoFocus)).observe(
+                        |_trigger: Trigger<ButtonActivate>,
+                         mut selected_mode: ResMut<SelectedGameRunMode>,
+                         mut next_state: ResMut<NextState<AppState>>| {
+                            selected_mode.0 = Some(GameRunMode::Game);
+                            next_state.set(AppState::ResetGameRun);
+                        },
+                    );
 
-                spawner.spawn(button("Training")).observe(
-                    |_trigger: Trigger<ButtonActivate>,
-                     mut selected_mode: ResMut<SelectedGameRunMode>,
-                     mut next_state: ResMut<NextState<AppState>>| {
-                        selected_mode.0 = Some(GameRunMode::Training);
-                        next_state.set(AppState::ResetGameRun);
-                    },
-                );
+                    spawner.spawn(button("Training")).observe(
+                        |_trigger: Trigger<ButtonActivate>,
+                         mut selected_mode: ResMut<SelectedGameRunMode>,
+                         mut next_state: ResMut<NextState<AppState>>| {
+                            selected_mode.0 = Some(GameRunMode::Training);
+                            next_state.set(AppState::ResetGameRun);
+                        },
+                    );
 
-                spawner.spawn(button("Level Select")).observe(
-                    |_trigger: Trigger<ButtonActivate>,
-                     mut selected_mode: ResMut<SelectedGameRunMode>,
-                     mut next_state: ResMut<NextState<AppState>>| {
-                        selected_mode.0 = Some(GameRunMode::SingleLevel);
-                        next_state.set(AppState::ResetGameRun);
-                    },
-                );
+                    spawner.spawn(button("Level Select")).observe(
+                        |_trigger: Trigger<ButtonActivate>,
+                         mut selected_mode: ResMut<SelectedGameRunMode>,
+                         mut next_state: ResMut<NextState<AppState>>| {
+                            selected_mode.0 = Some(GameRunMode::SingleLevel);
+                            next_state.set(AppState::ResetGameRun);
+                        },
+                    );
 
-                spawner.spawn(button("Quit")).observe(
-                    |_trigger: Trigger<ButtonActivate>, mut exit_event: EventWriter<AppExit>| {
-                        info!("Exiting");
-                        exit_event.write_default();
-                    },
-                );
+                    spawner.spawn(button("Quit")).observe(
+                        |_trigger: Trigger<ButtonActivate>,
+                         mut exit_event: EventWriter<AppExit>| {
+                            info!("Exiting");
+                            exit_event.write_default();
+                        },
+                    );
 
-                // TODO: Reenable once layout is closer to finished
-                // spawner.spawn((
-                //     Name::new("floating_borders"),
-                //     Node::default(),
-                //     Pickable::IGNORE,
-                // ));
-            })),
-        )],
+                    // TODO: Reenable once layout is closer to finished
+                    // spawner.spawn((
+                    //     Name::new("floating_borders"),
+                    //     Node::default(),
+                    //     Pickable::IGNORE,
+                    // ));
+                })),
+            )
+        ],
     ));
 }
